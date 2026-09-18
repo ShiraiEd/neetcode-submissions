48318de135e5bc8838356912635b721e10b37b85
Eduardo Egydio Shirai <eduardoegydioshirai599@gmail.com>
2026-09-18 15:24:02 +0900

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i, v in enumerate(nums):
+            gap = target -  v
+            if gap in hash:
+                return [hash[gap], i]
+            else :
+                hash[v] = i
+        return []
